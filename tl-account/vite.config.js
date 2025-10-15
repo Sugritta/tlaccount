12b4921044a901763dcf3cb3fda3d000cbeecb8c
Sugritta <mukchaemin21@gmail.com>
2025-10-15 12:04:43 +0700

    update vite config

--- tl-account/vite.config.js
+++ tl-account/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tlaccount/', // 👈 เพิ่มบรรทัดนี้
+  base: '/tlaccount/', // นี่คือการแก้ไขที่ถูกต้อง
 })
